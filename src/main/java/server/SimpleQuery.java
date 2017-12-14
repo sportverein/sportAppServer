95be628b284442cf122c3f97ba9781fd8b31e7f1
sportverein <seb@spvgg-ramspau.de>
2017-12-14 15:08:47 +0100

    login + off

--- src/main/java/server/SimpleQuery.java
+++ src/main/java/server/SimpleQuery.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 // autor: mm-geh
 
@@ -9,7 +10,10 @@ public class SimpleQuery {
 
     public static void main(String[] args) {
 
-       SimpleQuery.signoff("10.10.10.10", 2);
+        //SimpleQuery.signoff("10.10.10.10", 2);
+        //SimpleQuery.signon("10.10.10.10", 2);
+        //SimpleQuery.delivery("7a",2,3);
+        //DruckSimpleQuery.Rest(2);
     }
 
         // Verbindungs-daten
@@ -19,8 +23,9 @@ public class SimpleQuery {
     final static String user = "d028be11";
     final static String password = "alp-dillingen17";
 
+
     static Connection conn = null;
-    int i = 0;
+    static int i  = 3;
 
         public static void connect()
         {
@@ -48,7 +53,10 @@ public static boolean signon (String ip, int Station)
         //Datenübermittlung
         Statement stmt = conn.createStatement(); //Statement beginnt
         //Abfrage beginnen
-        String sqlCommand = "UPDATE station SET IP =  '"+ ip + "' WHERE Nr = " +Station+";";
+
+
+
+             String sqlCommand = "UPDATE station SET IP =  '"+ ip + "' WHERE Nr = " +Station+" AND IP = '';";
         stmt.executeUpdate(sqlCommand);
 
         stmt.close(); //Statement beenden
@@ -98,11 +106,11 @@ public  static boolean signoff (String ip, int Station)
         return true;
     } //Ende Signoff
 
-    public  static boolean Delivery (String ip, int Station)
+    public  static boolean delivery (String Klasse, int Station, int Punkte)
 
     {
         connect();
-
+        i++;
         try {                      //Verbindung aufbauen
 
             String url = "jdbc:mysql://"+hostname+":"+port+"/"+dbname;
@@ -110,7 +118,7 @@ public  static boolean signoff (String ip, int Station)
             //Datenübermittlung
             Statement stmt = conn.createStatement(); //Statement beginnt
             //Abfrage beginnen
-            String sqlCommand = "UPDATE station SET IP = '' " + " WHERE Nr = " +Station+ " AND IP = '"+ ip + "';";
+            String sqlCommand = "INSERT INTO klasse_zu_station (Klasse,Station,Punkte,ID) VALUES ('"+ Klasse +"',"+Station+","+ Punkte +","+ i +");";
             stmt.executeUpdate(sqlCommand);
 
             stmt.close(); //Statement beenden
@@ -127,5 +135,60 @@ public  static boolean signoff (String ip, int Station)
 
         return true;
     } //Ende Signoff
+
+
+    public static ArrayList grouplist (int Station)
+
+    {
+     ArrayList<String> Dropdown = new ArrayList<>();
+
+         connect();
+
+        String output ="";
+        try {
+            //Verbindung aufbauen
+            String url = "jdbc:mysql://"+hostname+":"+port+"/"+dbname;
+            conn = DriverManager.getConnection(url, user, password);
+            //Datenübermittlung
+            Statement stmt = conn.createStatement(); //Statement beginnt
+            //Abfrage beginnen
+
+            String sqlCommand = "SELECT Name FROM klassen WHERE Name NOT IN (SELECT Klasse FROM klasse_zu_station WHERE Station = "+ Station + " AND Punkte >= 0);";
+
+            ResultSet rs = stmt.executeQuery(sqlCommand);
+
+
+            while (rs.next())
+            {
+                Dropdown.add(rs.getString(1));
+
+            }
+
+
+            for (int i = 0;i<Dropdown.size();i++)
+            {
+                output = output +(Dropdown.get(i)+",");
+            }
+
+
+            stmt.close(); //Statement beenden
+            conn.close(); // Datenbank-Verbindung beenden
+        }
+        catch (SQLException sqle) {
+            System.out.println("SQLException: " + sqle.getMessage());
+            System.out.println("SQLState: " + sqle.getSQLState());
+            System.out.println("VendorError: " + sqle.getErrorCode());
+            sqle.printStackTrace();
+
+            return null;
+        }
+
+
+        return Dropdown;
+
+    }
+
+
+
 
 } //Ende Klasse
