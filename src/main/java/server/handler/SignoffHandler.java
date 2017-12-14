0a94ff4a38d0349057e3661f160e9b637f686985
sportVerein <seb@spvgg-ramspau.de>
2017-12-14 16:53:32 +0100

    Signoff

--- src/main/java/server/handler/SignoffHandler.java
+++ src/main/java/server/handler/SignoffHandler.java
@@ -1,13 +1,13 @@
 package server.handler;
 
 import server.SimpleQuery;
+import server.protocol.SignoffRequest;
 import server.protocol.SignoffResponse;
-import server.protocol.SignonRequest;
 
 public class SignoffHandler extends BaseHandler {
     public String handle(String ip, String json){
 
-        SignonRequest signonRequest = (SignonRequest) deserialize(json, SignonRequest.class);
+        SignoffRequest signonRequest = (SignoffRequest) deserialize(json, SignoffRequest.class);
 
         boolean success = SimpleQuery.signoff(ip, signonRequest.getStation());
 
