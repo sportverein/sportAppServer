0bca5b3185a1c3a5b096d1bc31491ecb0ed264ac
sportverein <seb@spvgg-ramspau.de>
2017-12-14 15:34:35 +0100

    Grouplist

--- src/main/java/server/handler/GrouplistHandler.java
+++ src/main/java/server/handler/GrouplistHandler.java
@@ -9,13 +9,15 @@ import server.protocol.SignonResponse;
 import java.util.ArrayList;
 
 public class GrouplistHandler extends BaseHandler{
+
     public String handle(int station, String json){
 
-        GrouplistRequest grouplistRequest = (GrouplistRequest) deserialize(json, GrouplistRequest.class));
+        GrouplistRequest grouplistRequest = (GrouplistRequest) deserialize(json, GrouplistRequest.class);
 
         ArrayList<String> grouplist = SimpleQuery.grouplist(grouplistRequest.getStation());
 
         GrouplistResponse grouplistResponse = new GrouplistResponse(grouplist, true);
 
         return serialize(grouplistResponse);
+    }
 }
